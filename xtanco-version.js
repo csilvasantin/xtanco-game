5171b64ed469ecbb2d4bd804cc7a46cd94372734
csilvasantin <csilvasantin@MacMini.local>
2026-04-16 23:30:53 +0200

    Actualización automática 2026-04-16 23:29

--- xtanco-version.js
+++ xtanco-version.js
@@ -3,7 +3,7 @@
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
     version: 'v9.16',
-    build: '20260416-2243',
-    cacheName: 'admiraxp-v9-16-20260416-2243',
+    build: '20260416-2329',
+    cacheName: 'admiraxp-v9-16-20260416-2329',
   });
 })();
