29309784f3a144861787fe7ded34c98c616ba723
csilvasantin <csilvasantin@MacMini.local>
2026-04-19 22:02:51 +0200

    feat: add supermarket selector flow

--- xtanco-version.js
+++ xtanco-version.js
@@ -2,8 +2,8 @@
   const root = typeof self !== 'undefined' ? self : window;
   root.XTANCO_APP = Object.freeze({
     name: 'Admira XP // The Xpace OS',
-    version: 'v9.16',
-    build: '20260416-2344',
-    cacheName: 'admiraxp-v9-16-20260416-2344',
+    version: 'AdmiraNext v.26.19.04.1',
+    build: '20260419-2124',
+    cacheName: 'admiranext-v-26-19-04-1-20260419-2124',
   });
 })();
